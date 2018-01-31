552f3d8c0c87eb4aa5ef058b0f868602d03390f4
mdubourg <mdubourg@akema.fr>
2018-01-31 16:09:14 +0100

    lints & comments

--- src/components/model.js
+++ src/components/model.js
@@ -2,21 +2,30 @@
 export class Model {
 
   constructor(mock_url) {
+    /* les datas en JSON comme on les récupère sur de la requête */
     this.raw_data = JSON.parse(http_get(mock_url));
+    /* sera notre set de données après ordering et filtering */
     this.filtered_data = this.raw_data;
     this.columns = Object.keys(this.raw_data[0]);
 
+    /* décalage des lignes affichées par rapport à la première ligne du dataset */
     this.offset = 0;
+    /* nombre d'éléments affichés à par page */
     this.range = 10;
 
+    /* index de pagination actuellement séléctionné */
     this.selected_index = 0;
+    /* data actuellement affichées */
     this.displayed_data = this.filtered_data.slice(0, this.range);
 
+    /* contenu de la searchbar */
     this.searchbar = "";
-    this.ordering = this.columns[0];
-    this.order(this.ordering);
+    /* nom de la colonne selon laquelle on order */
+    this.ordering = "";
+    this.order(this.columns[0]);
   }
 
+  /* callback lors de l'appui sur 'Suivant' (pagination) */
   next() {
     if (this.offset + this.range < this.filtered_data.length) {
       this.offset += this.range;
@@ -25,6 +34,7 @@ export class Model {
     }
   }
 
+  /* callback lors de l'appui sur 'Précédent' (pagination) */
   previous() {
     if (this.offset - this.range >= 0) {
       this.offset -= this.range;
@@ -33,12 +43,14 @@ export class Model {
     }
   }
 
+  /* callback lors de l'appui sur un index (pagination) */
   goto_index(index) {
     this.offset = this.range * index;
     this.selected_index = index;
     this.displayed_data = this.filtered_data.slice(this.offset, this.offset + this.range);
   }
 
+  /* callback lors du changement de model.range */
   update_range() {
     this.range = parseInt(this.range);
     this.offset = 0;
@@ -46,7 +58,7 @@ export class Model {
     this.displayed_data = this.filtered_data.slice(0, this.range);
   }
 
-
+  /* filtrage lors de l'input dans la barre de recherche */
   filter() {
     this.selected_index = 0;
     this.offset = 0;
@@ -64,6 +76,7 @@ export class Model {
     this.order(this.ordering);
   }
 
+  /* ordering au clic sur le nom des colonnes */
   order(column) {
     if (this.ordering === column && this.ordering.slice(0, 1) === '-')
       this.ordering = this.ordering.slice(1);
@@ -93,6 +106,9 @@ export class Model {
 }
 
 
+/* =========== FONCTIONS UTILITAIRES ========== */
+
+/* réalise une requête HTTP GET et retourne le résultat */
 function http_get(url) {
   let xhttp = new XMLHttpRequest();
   xhttp.open("GET", url, false);

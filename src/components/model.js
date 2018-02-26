b8848a9c7c2b6b9a7057d27db45ee23dfee59c35
mdubourg <mdubourg@akema.fr>
2018-02-26 17:50:56 +0100

    final push

--- src/components/model.js
+++ src/components/model.js
@@ -1,7 +1,7 @@
 export class Model {
 
   constructor(mock_url) {
-    /* les datas en JSON comme on les récupère sur de la requête */
+    /* les datas en JSON comme on les récupère de la requête */
     this.raw_data = JSON.parse(Model.http_get(mock_url));
     /* sera notre set de données après ordering et filtering */
     this.filtered_data = this.raw_data;
